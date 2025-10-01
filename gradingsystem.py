833f97c8b3cbdfebf31193d6ec6b3258edd5c7b2
Faheem Jabbar <faheemjabbar326@gmail.com>
2025-10-01 22:18:09 +0500

    Added three Files

---
+++ gradingsystem.py
@@ -0,0 +1,13 @@
+Marks = float(input("Enter your marks: "))
+if Marks >= 90:
+    print("A+")
+elif Marks >= 80:
+    print("A")
+elif Marks >= 70:
+    print("B+")
+elif Marks >= 60:
+    print("B")
+elif Marks >= 50:
+    print("C+")
+else:
+    print("Fail")
